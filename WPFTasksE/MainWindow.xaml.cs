5afa47f90732a878ac3abd490b097e66c41bb0f8
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-15 09:22:51 +0100

    Tasks and WPF

--- WPFTasksE/MainWindow.xaml.cs
+++ WPFTasksE/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,54 @@ namespace WPFTasksE
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty HtmlProperty =
+            DependencyProperty.RegisterAttached("Html", typeof(string), typeof(MainWindow), new FrameworkPropertyMetadata(OnHtmlChanged));
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId}.");
+                HttpClient webClient = new HttpClient();
+                string html = webClient.GetStringAsync("https://google.com").Result;
+
+                MyButton.Dispatcher.Invoke(() =>
+                {
+                    Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId}.");
+                    MyButton.Content = "Done";
+                });
+            });
+        }
+
+        private async void MyButton_Click2(object sender, RoutedEventArgs e)
+        {
+            string myHtml = "Bla";
+
+            Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} before await task.");
+            await Task.Run(() =>
+            {
+                Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} during await task.");
+                HttpClient webClient = new HttpClient();
+                string html = webClient.GetStringAsync("https://google.com").Result;
+                myHtml = html;
+            });
+            Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} after await task.");
+            MyButton.Content = "Done Downloading";
+            MyWebBrowser.SetValue(HtmlProperty, myHtml);
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser webBrowser = dependencyObject as WebBrowser;
+            if (webBrowser != null)
+            {
+                webBrowser.NavigateToString(e.NewValue as string);
+            }
         }
     }
 }
